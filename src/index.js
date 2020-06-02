448e07f6e7d90d9aa6472da478a55eafa0bf4eef
Maciej Uberna <maciej.uberna@gmail.com>
2020-06-02 17:53:22 +0200

    Lesson474 "Using the Custom Store" productsStore initialized in index.js .

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import ProductsProvider from './context/products-context';
+import configureProducsStore from './hooks-store/products-store';
+//that's how store is initialized
+configureProducsStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
